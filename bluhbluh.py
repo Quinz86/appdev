e7add93c027dc5b651b4bf2eae8ce7c320e7edcf
Quinz86 <enderstorm86@gmail.com>
2018-08-13 12:18:09 -0400

    fj;lakdjf

--- bluhbluh.py
+++ bluhbluh.py
@@ -1,10 +1,3 @@
-x = 0
-#this should be a python file
-#yes this is a python file
-for y in range(0,100):
-    print y
-
-import math
-print math.pi
-
-print "ooooh some new interesting twigs"
+bad
+nobody likes python
+no one like at all
